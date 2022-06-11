99002d48129628991dde3336698b0971ea9b8ded
GeorgeWL <hello@georgewl.dev>
2022-06-11 22:38:10 +0100

    footer text and end text

--- src/components/Texts.jsx
+++ src/components/Texts.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import List from './List';
+import { Footer } from './styled/Footer';
+import { Strong, StrongUpper } from './styled/Strong';
 
 export const OutlineText = () => (
   <div>
@@ -13,7 +15,7 @@ export const OutlineText = () => (
       items={[
         'One: You have a complex plan that requires precise timing.',
         <>
-          Two: You are a <strong>GODDAMN BEAR.</strong>
+          Two: You are a <StrongUpper>GODDAMN BEAR.</StrongUpper>
         </>,
       ]}
     />
@@ -31,31 +33,55 @@ export const ChangingStatesText = () => (
   <div>
     <h3>Stats</h3>
     <p>
-      <strong>Frustration:</strong> When the plan fails and you run into
-      difficulty, move one point from Criminal into Bear.{' '}
-      <em>(+1 Bear &amp; -1 Criminal)</em>
+      <Strong>Frustration:</Strong> When the plan fails and you run into
+      difficulty, move one point from <StrongUpper>Criminal</StrongUpper> into{' '}
+      <StrongUpper>Bear</StrongUpper>.{' '}
+      <em>
+        (+1 <StrongUpper>Bear</StrongUpper> &amp; -1{' '}
+        <StrongUpper>Criminal</StrongUpper>)
+      </em>
     </p>
     <p>
-      <strong>Greed:</strong> When the plan goes off without a hitch, move one
-      point from Bear into Criminal. <em>(-1 Bear &amp; +1 Criminal)</em>
+      <Strong>Greed:</Strong> When the plan goes off without a hitch, move one
+      point from <StrongUpper>Bear</StrongUpper> into{' '}
+      <StrongUpper>Criminal</StrongUpper>.{' '}
+      <em>
+        (-1 <StrongUpper>Bear</StrongUpper> &amp; +1{' '}
+        <StrongUpper>Criminal</StrongUpper>)
+      </em>
     </p>
     <p>
-      You can voluntarily move one point from Bear to Criminal by doing a
-      flashback scene in which you and the other bears plan out the heist over
-      coffee and cigarrettes in the back room of a seedy bar.
+      You can voluntarily move one point from <StrongUpper>Bear</StrongUpper> to{' '}
+      <StrongUpper>Criminal</StrongUpper> by doing a flashback scene in which
+      you and the other bears plan out the heist over coffee and cigarrettes in
+      the back room of a seedy bar.
     </p>
     <p>
-      You can voluntarily move one point of Criminal into Bear by eating a load
-      of honey.
+      You can voluntarily move one point of <StrongUpper>Criminal</StrongUpper>{' '}
+      into <StrongUpper>Bear</StrongUpper> by eating a load of honey.
     </p>
   </div>
 );
-const FooterStyled = styled.footer`
-  margin-top: 20px;
-`;
 
+export const EndText = () => (
+  <div>
+    <h3>The End</h3>
+    <p>
+      If your <StrongUpper>Criminal</StrongUpper> stat ever reaches 6, you are
+      lured into a life of crime and betray the party.
+    </p>
+    <p>
+      If your <StrongUpper>Bear</StrongUpper> stat ever reaches 6, you flip out
+      bear-style and lose it. Presumably to be picked up by animal control in
+      half an hour or so.
+    </p>
+  </div>
+);
 export const FooterText = () => (
-  <FooterStyled>
-    Companion for Honey Heist by Grant Howitt. This app is made by GeorgeWL
-  </FooterStyled>
+  <Footer>
+    Companion for Honey Heist by Grant Howitt. This app is made by{' '}
+    <a href="https://georgewl.dev" target="_blank" rel="noreferrer">
+      GeorgeWL
+    </a>
+  </Footer>
 );
